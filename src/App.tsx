a0333e5171d9b7c1f0b6b0072a6790b4c1334e7f
Yaroslav Shabelnyk <62596807+bynarig@users.noreply.github.com>
2024-11-18 17:28:27 +0100

    initial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Contact from './components/Contacts';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Contact />
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  darkMode: 'class',  // This enables the class-based dark mode
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
